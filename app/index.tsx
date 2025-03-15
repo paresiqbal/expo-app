b34c3ae837dd05d288b53db193696b84a0dc5852
paresiqbal <thecrowdel@gmail.com>
2025-03-15 13:48:52 +0700

    update layout and index components; enhance styling and add NativeWind support

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-teal-500 font-bold">Hello World!</Text>
     </View>
   );
 }
