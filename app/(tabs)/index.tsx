2c72eb04f29e37f764b9b8de0c66b6931d1b4522
paresiqbal <thecrowdel@gmail.com>
2025-03-16 15:21:54 +0700

    add SearchBar component and integrate it into the main tab layout for improved movie search functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
+
+// constants
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Link } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+
+// components
+import SearchBar from "@/components/SearchBar";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-gray-800">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -13,6 +20,13 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for movies"
+          />
+        </View>
       </ScrollView>
     </View>
   );
