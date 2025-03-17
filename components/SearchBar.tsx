a253af7ff4fc9183f37bfe8d450f6d07caaac186
paresiqbal <thecrowdel@gmail.com>
2025-03-17 22:38:25 +0700

    refactor SearchBar component to make value and onChangeText optional with default values

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,11 +5,16 @@ import { icons } from "@/constants/icons";
 interface Props {
   placeholder: string;
   onPress?: () => void;
-  value: string;
-  onChangeText: (text: string) => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
+const SearchBar = ({
+  placeholder,
+  onPress,
+  value = "",
+  onChangeText = () => {},
+}: Props) => {
   return (
     <View className="flex-row items-center bg-gray-400 rounded-full px-5 py-4">
       <Image
